453c6b72ab368694f44688038df19dd8073ab028
Martin <mszauer@mail.usf.edu>
2015-06-19 21:54:10 -0700

    functional sprite draw, and added assets to file

--- SpriteSystem/ProtoGameBase.cs
+++ SpriteSystem/ProtoGameBase.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Game;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace Game {
     class ProtoGameBase : GameBase{
+        Sprite sprite = null;
         public ProtoGameBase(){
+            sprite = new Sprite("Assets\\flappyBat.png");
+        }
+
+        public override void Render(Graphics g) {
+            sprite.Draw(g, new Rect(new Point(0, 0), new Point(width, height)), new Rect(new Point(0, 0), new Point(285, 510)));
         }
     }
 }
--- SpriteSystem/Sprite.cs
+++ SpriteSystem/Sprite.cs
@@ -10,8 +10,23 @@ using System.Drawing.Drawing2D;
 
 namespace Game {
     class Sprite{
-        public Sprite() {
-            
+        Image spriteSheet = null;
+
+        public Sprite(string filepath) {
+            spriteSheet = Image.FromFile(filepath);
         }
+        public void Draw(Graphics g,Point p) {
+            g.DrawImage(spriteSheet, p);
+        }
+
+        public void Draw(Graphics g,Point p, Rect subSprite) {
+            Rect r = new Rect(p,new Size((Int32)subSprite.W,(Int32)subSprite.H));
+            g.DrawImage(spriteSheet, r.Rectangle, subSprite.Rectangle, GraphicsUnit.Pixel);
+        }
+
+        public void Draw(Graphics g, Rect screen, Rect subSprite) {
+            g.DrawImage(spriteSheet, screen.Rectangle, subSprite.Rectangle, GraphicsUnit.Pixel);
+        }
+
     }
 }
