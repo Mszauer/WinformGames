e48be82c1cda4b8280a8cf842346b69d3156e2b6
Martin <mszauer@mail.usf.edu>
2015-06-20 16:37:17 -0700

    started sprite class, added animation and movement

---
+++ SpriteSystem/ProtoAnimation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace Game {
+    class ProtoAnimation {
+        List<Rect> subSprites = null;
+        Image spriteSheet = null;
+        float timeAccum = 0;
+        float updateRate = 0;
+        int spriteIndex = 0;
+        public Size spriteSize {
+            get {
+                return new Size((Int32)subSprites[spriteIndex].W,
+                (Int32)subSprites[spriteIndex].H);
+
+            }
+        }
+
+
+
+        public ProtoAnimation(string filePath, float fps) {
+            updateRate = 1.0f / fps;
+            subSprites = new List<Rect>();
+            using (StreamReader sr = new StreamReader(filePath)){
+                string line;
+                while ((line = sr.ReadLine()) != null) {
+                    if (line[0] == 'R'){
+                        string[] split = line.Split(new Char [] {' '});
+                        Rect r = new Rect(System.Convert.ToInt32(split[1]),System.Convert.ToInt32(split[2]),System.Convert.ToInt32(split[3]),System.Convert.ToInt32(split[4]));
+                        subSprites.Add(r);
+                    }
+                    else if (line[0] == 'I'){
+                        string[] split = line.Split(new Char[] {' '});
+                        spriteSheet = Image.FromFile(split[1]);
+                    }
+                }
+            }
+        }
+
+        public void Update(float dTime) {
+            timeAccum += dTime;
+            if (timeAccum >= updateRate) {
+                spriteIndex++;
+                if (spriteIndex >= subSprites.Count) {
+                    spriteIndex = 0;
+
+                }
+                timeAccum -= updateRate;
+            }
+            
+        }
+
+        public void Render(Graphics g, Point p) {
+            Rect r = new Rect(p, new Size((Int32)subSprites[spriteIndex].W, (Int32)subSprites[spriteIndex].H));
+            g.DrawImage(spriteSheet, r.Rectangle,subSprites[spriteIndex].Rectangle, GraphicsUnit.Pixel);
+        }
+    }
+}
--- SpriteSystem/ProtoGameBase.cs
+++ SpriteSystem/ProtoGameBase.cs
@@ -10,12 +10,30 @@ using System.Drawing;
 namespace Game {
     class ProtoGameBase : GameBase{
         Sprite sprite = null;
-        public ProtoGameBase(){
+        ProtoAnimation animatedSprite = null;
+        ProtoAnimation marioRun = null;
+        Point spritePosition = default(Point);
+
+        public override void Initialize(){
             sprite = new Sprite("Assets\\flappyBat.png");
+            animatedSprite = new ProtoAnimation("Assets/gohanrects.txt", 30f);
+            marioRun = new ProtoAnimation("Assets/marioRun.txt",30f);
+            spritePosition.X = width - marioRun.spriteSize.Width - 1;
+        }
+
+        public override void Update(float deltaTime) {
+            animatedSprite.Update(deltaTime);
+            marioRun.Update(deltaTime);
+            if(spritePosition.X > 0){
+                spritePosition.X -= (Int32)(30f * deltaTime);
+            }
+            
         }
 
         public override void Render(Graphics g) {
             sprite.Draw(g, new Rect(new Point(0, 0), new Point(width, height)), new Rect(new Point(0, 0), new Point(285, 510)));
+            animatedSprite.Render(g, new Point(width / 2, height / 2));
+            marioRun.Render(g, new Point(spritePosition.X, height - marioRun.spriteSize.Height));
         }
     }
 }
