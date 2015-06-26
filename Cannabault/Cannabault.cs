63acfa176fa609d62c34e2a5259c2e3cb873acc3
Martin <mszauer@mail.usf.edu>
2015-06-26 14:20:05 -0700

    added obstacle logic, bldgs generated/scroll/respawn

--- Cannabault/Cannabault.cs
+++ Cannabault/Cannabault.cs
@@ -8,10 +8,45 @@ using System.Drawing;
 
 namespace Game {
     class Cannabault : GameBase{
+        Obstacle bldg1 = null;
+        Obstacle bldg2 = null;
+        Obstacle bldg3 = null;
+        
 
         public Cannabault() {
             width = 400;
             height = 600;
+        }
+
+        public override void Initialize() {
+            bldg1 = new Obstacle(new Size(width, height));
+            bldg2 = new Obstacle(new Size(width, height));
+            bldg3 = new Obstacle(new Size(width, height));
+            bldg1.lastBuilding = bldg3;
+            bldg2.lastBuilding = bldg1;
+            bldg3.lastBuilding = bldg2;
+
+            bldg1.Initialize(0);
+            bldg2.Initialize(bldg2.lastBuilding.X+bldg2.lastBuilding.W +bldg2.bldgSpacing);
+            bldg3.Initialize(bldg3.lastBuilding.X+bldg3.lastBuilding.W+bldg3.bldgSpacing);
+        }
+
+        public override void Update(float dTime) {
+            bldg1.Update(dTime);
+            bldg2.Update(dTime);
+            bldg3.Update(dTime);
+        }
+
+        public override void Render(Graphics g) {
+            bldg1.Render(g,Brushes.Red);
+            bldg2.Render(g, Brushes.Green);
+            bldg3.Render(g, Brushes.Blue);
+        }
+
+        void DebugControls() {
+            if (KeyPressed(Keys.R)) {
+                bldg1.Initialize(0);
+            }
         }
     }
 }
---
+++ Cannabault/Obstacle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Game {
+    class Obstacle {
+        Rect building = null;
+        public Obstacle lastBuilding = null;
+        public float speed = 100;
+        Random r = null;
+        public float bldgSpacing = 0;
+        Size windowWH = default(Size);
+        public float X {
+            get {
+                return building.X;
+            }
+            set {
+                building.X = value;
+            }
+        }
+        public float W {
+            get {
+                return building.W;
+            }
+        }
+
+        public Obstacle(Size window) {
+            windowWH = window;
+            r = new System.Random(Guid.NewGuid().GetHashCode());
+        }
+
+        public void Initialize(float x) {
+            float bldgWidth = r.Next(150, windowWH.Width - 25); // 150min width, 300 max width
+            float bldgHeight = r.Next(300, windowWH.Height - 250); //350min height, 300max height (in y terms)
+            bldgSpacing = r.Next(50, 150);
+#if DEBUG
+            Console.WriteLine("Width: " + bldgWidth);
+            Console.WriteLine("Height: " + bldgHeight);
+            Console.WriteLine("Spacing: " + bldgSpacing);
+#endif
+            building = new Rect(x, bldgHeight, bldgWidth, windowWH.Height);
+        }
+
+        public void Update(float dTime) {
+            building.X -= dTime * speed;
+            if (building.X < 0 - building.W) {
+                // dont set the building x just generate new one?
+                //building.X = lastBuilding.X + lastBuilding.W + bldgSpacing ;
+                Initialize(lastBuilding.X + lastBuilding.W + bldgSpacing);
+            }
+
+
+        }
+
+        public void Render(Graphics g, Brush brushColor) {
+#if DEBUG
+            DebugRender(g, brushColor);
+#endif
+        }
+
+        void DebugRender(Graphics g, Brush brushColor) {
+            g.FillRectangle(brushColor, building.Rectangle);
+
+        }
+    }
+}
