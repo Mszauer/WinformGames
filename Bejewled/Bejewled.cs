473152effe687a4b7837d7cc603ea29ef9aa559f
Martin <mszauer@mail.usf.edu>
2015-06-29 16:46:02 -0700

    added base skeleton for bejewled

---
+++ Bejewled/Bejewled.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+
+namespace Game {
+    class Bejewled : GameBase{
+        public Bejewled() {
+            width = 400;
+            height = 600;
+        }
+    }
+}
---
+++ Bejewled/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game {
+    class Program {
+        static void Main(string[] args) {
+            Application.Run(new GameWindow(new Bejewled()));
+        }
+    }
+}
