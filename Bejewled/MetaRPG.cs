2d6c7fb743e0ea59931500f0622969bb0be3e72d
Martin <mszauer@mail.usf.edu>
2015-07-05 12:35:20 -0700

    added hud

--- Bejewled/MetaRPG.cs
+++ Bejewled/MetaRPG.cs
@@ -10,9 +10,14 @@ using System.Drawing;
 namespace Game {
     class MetaRPG {
         List<Sprite> rpgBackGround = null;
+        Sprite portrait = null;
+        Sprite navigation = null;
+        Sprite healthBar = null;
+        List<Sprite> healthColors = null;
 
         public MetaRPG() {
             rpgBackGround = new List<Sprite>();
+            healthColors = new List<Sprite>();
         }
 
         public void Initialize() {
@@ -20,6 +25,12 @@ namespace Game {
                 Sprite bg = new Sprite("Assets/w_" + i + ".png");
                 rpgBackGround.Add(bg);
             }
+            portrait = new Sprite("Assets/portrait.png");
+            navigation = new Sprite("Assets/navigation.png");
+            healthBar = new Sprite("Assets/health.png");
+            healthColors.Add(new Sprite("Assets/red.png"));
+            healthColors.Add(new Sprite("Assets/yellow.png"));
+            healthColors.Add(new Sprite("Assets/green.png"));
         }
 
         public void Update(float dTime) {
@@ -27,6 +38,10 @@ namespace Game {
         }
         public void Render(Graphics g) {
             rpgBackGround[0].Draw(g, new Point(0, 0));
+            portrait.Draw(g, new Point(355, 0));
+            navigation.Draw(g, new Point(355, 141));
+            healthBar.Draw(g, new Point(355, 124));
+            //health colors
         }
     }
 }
