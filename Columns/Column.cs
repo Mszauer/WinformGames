b191c257a6ce3f1944a5d6ab20fafbfae3efe004
Martin <mszauer@mail.usf.edu>
2015-07-14 09:06:32 -0700

    started working on columns game, creating column object

---
+++ Columns/Column.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game {
+    class Column {
+        int tileSize = 0; //set from ColumnWindow tileSize
+        List<List<Rect>> columnStates = null;
+        public Point Position = default(Point); //gets set in ColumnWindow and accounts for offset
+        //checks to see what order the column is in, and can set or return it
+        int currentState = 0;
+        public int CurrentState {
+            get {
+                return currentState;
+            }
+            set {
+                currentState = value;
+            }
+        }
+        public Rect AABB { //Axis Aligned Boundry Block
+            get {
+                float minX = columnStates[currentState][0].X;
+                float minY = columnStates[currentState][0].Y;
+                float maxX = columnStates[currentState][0].X + columnStates[currentState][0].W;
+                float maxY = columnStates[currentState][0].Y + columnStates[currentState][0].H;
+
+            }
+        }
+
+        public Column(int size) {
+            tileSize = size;
+        }
+        public void Initialize() {
+            //create new list to use
+            columnStates = new List<List<Rect>>();
+        }
+        public void CreateColumn(int[][] rowcol) {
+            //create Individual columns
+            List<Rect> shape = new List<Rect>();
+            //loop through rows and columns passed in
+            for (int col = 0; col < rowcol.Length; col++) {
+                for (int row = 0; row < rowcol[col].Length; row++) {
+                    //if value passed in >0 create a rect
+                    if (rowcol[row][col] > 0) {
+                        //create a rect in pixel space
+                        Rect r = new Rect(col * tileSize, row * tileSize, tileSize, tileSize);
+                        //add rect to shape (to form a shape)
+                        shape.Add(r);
+                    }//end if
+                }//end row
+            }//end col
+            columnStates.Add(shape);
+        }
+        public void Switch() {
+            //create a loop for switching rects in a column
+            currentState++;
+            if (currentState >= columnStates.Count) {
+                currentState = 0;
+            }
+        }
+    }
+}
---
+++ Columns/ColumnWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game {
+    class ColumnWindow : GameBase{
+        int[][] logicBoard = null;
+        int boardW = 0; // board width
+        int boardH = 0; // board Height
+        int tileSize = 50; //size of tiles
+        int xOffset = 0;
+        int yOffset = 0;
+
+        public ColumnWindow(int w=5,int h=5, int xOffset = 0, int yOffset = 0) {
+            width = 400;
+            height = 600;
+            title = "Columns";
+            boardW = w;
+            boardH = h;
+            this.xOffset = xOffset;
+            this.yOffset = yOffset;
+        }
+        public void Reset() {
+            //set up logic board size and set values to default(0);
+            logicBoard = new int[boardW][];
+            for (int i = 0 ; i < logicBoard.Length ; i++){
+                logicBoard[i] = new int[boardH];
+            }
+            
+        }
+        public override void Initialize() {
+            Reset();
+        }
+        public override void Update(float deltaTime) {
+            base.Update(deltaTime);
+        }
+        public override void Render(Graphics g) {
+            //draw logic board
+            using (Pen p = new Pen(Brushes.Black, 1f)) {
+                for (int col = 0; col < logicBoard.Length; col++) {
+                    //Draw columns
+                    g.DrawLine(p, new Point(col * tileSize+xOffset, yOffset), new Point(col * tileSize+xOffset, logicBoard[col].Length * tileSize+yOffset));
+                    
+                    for (int row = 0; row < logicBoard[col].Length; row++) {
+                        //draw rows
+                        g.DrawLine(p, new Point(xOffset, row * tileSize+yOffset), new Point(xOffset+logicBoard[col].Length*tileSize, row*tileSize+yOffset));
+                    }//end col
+                }//end row
+                //Draw last lines to close the square
+                g.DrawLine(p, new Point(xOffset, (boardH) * tileSize + yOffset), new Point(boardH*tileSize+xOffset,(boardW) * tileSize + yOffset));
+                g.DrawLine(p, new Point(xOffset + boardW * tileSize, yOffset), new Point(xOffset + boardW * tileSize, boardH * tileSize));
+            }//end using pen p
+        }
+        
+    }
+}
