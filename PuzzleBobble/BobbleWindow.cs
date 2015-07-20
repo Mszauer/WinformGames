51aed4d880bd67cd906a0e0b811480caa2b57e52
Martin <mszauer@mail.usf.edu>
2015-07-19 20:48:31 -0700

    resumed puzzle bobble stuck on rendering grid

--- PuzzleBobble/BobbleWindow.cs
+++ PuzzleBobble/BobbleWindow.cs
@@ -8,84 +8,22 @@ using System.Drawing;
 using System.Windows.Forms;
 
 namespace Game {
-    class BobbleWindow : GameBase {
-        Hexagon[][] board = null;
-        Hexagon[][] board2 = null;
-        float hexRadius = 20.0f; // set size of hexagons
-        int boardWidth = 5; // # of hexagons on x axis
-        int boardHeight = 5; // # of hexagons on y axis
-        float xOffset = 0f;
-        float yOffset = 0f;
-        float xOffset2 = 0f;
-        float yOffset2 = 300f;
+    class BobbleWindow : GameBase{
+        PuzzleBobble bobble = null;
 
         public BobbleWindow() {
             width = 400;
             height = 600;
-            //set up board
-            board = new Hexagon[boardWidth][];
-            for (int i = 0; i < board.Length; i++) {
-                board[i] = new Hexagon[boardHeight];
-            } 
-            board2 = new Hexagon[boardWidth][];
-            for (int i = 0; i < board.Length; i++) {
-                board2[i] = new Hexagon[boardHeight];
-            }
+            bobble = new PuzzleBobble(new Point(0,0),new Size(6,7));//Offset,Board Size (x*y), hexagonal radius
         }
         public override void Initialize() {
-            for (int x = 0; x < board.Length; x++) {
-                for (int y = 0; y < board[x].Length; y++) {
-                    Hexagon H = new Hexagon(hexRadius,xOffset,yOffset);
-                    H.xIndexer = x;
-                    H.yIndexer = y;
-                    board[x][y] = H;
-                }
-            }
-            for (int x = 0; x < board2.Length; x++) {
-                for (int y = 0; y < board2[x].Length; y++) {
-                    Hexagon H = new Hexagon(hexRadius, xOffset2, yOffset2);
-                    H.xIndexer = x;
-                    H.yIndexer = y;
-                    board2[x][y] = H;
-                }
-            }
+            bobble.Initialize();
         }
-        public override void Update(float deltaTime) {
-
+        public override void Update(float dTime) {
+            bobble.Update(dTime);
         }
-
-        public void HighlightNeighbors(Graphics g,float xOffset, float yOffset, Hexagon[][] board) {
-            // Get the X, Y (board indices) of the mouse position
-            Point mouseIndex = Hexagon.TileAt(new Point(MousePosition.X,MousePosition.Y), hexRadius,xOffset,yOffset);
-             // Make sure mouseIndex is in bounds
-            if (mouseIndex.X >= 0 && mouseIndex.Y >= 0 && mouseIndex.X < boardWidth && mouseIndex.Y < boardHeight) {
-                // Get all neighbors
-                List<Point> neighbors = board[mouseIndex.X][mouseIndex.Y].GetNeighborIndixes();
-                // Loop trough all neighbors
-                foreach (Point p in neighbors) {
-                    // If neightbor is valid
-                    if (p.X >= 0 && p.Y >= 0 && p.X < boardWidth && p.Y < boardHeight) {
-                        // Draw neighbor
-                        board[p.X][p.Y].Draw(g, Pens.Green);
-
-                        Point nw = board[mouseIndex.X][mouseIndex.Y].GetNeighborIndex(Hexagon.Directions.NorthWest);
-                        if (p == nw) {
-                            board[nw.X][nw.Y].Draw(g, Pens.Yellow);
-                        }
-                    }
-                }
-                board[mouseIndex.X][mouseIndex.Y].Draw(g, Pens.Red);
-            }
-        }
-
-        public override void Render(Graphics g){
-            //Draw Board
-            for (int x = 0; x < board.Length; x++) {
-                for (int y = 0; y < board[x].Length; y++) {
-                    board[x][y].Draw(g, Pens.Blue);
-                }
-            }
-            HighlightNeighbors(g, xOffset, yOffset, board);
+        public override void Render(Graphics g) {
+            bobble.Render(g);
         }
     }
 }
---
+++ PuzzleBobble/PuzzleBobble.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game {
+    class PuzzleBobble {
+        public Hexagon[][] Board = null;
+        public Size BoardDimensions = default(Size);
+        Point boardOffset = default(Point);
+        float BallRadius {
+            get {
+                return Board[0][0].HalfW;
+            }
+        }
+        float BallDiameter {
+            get {
+                return BallRadius * 2;
+            }
+        }
+        Rect BoardArea {
+            get {
+                return new Rect(boardOffset.X, boardOffset.Y, BoardDimensions.Width * BallDiameter, BoardDimensions.Height * BallRadius + 150);
+            }
+        }
+        Point BoardCenter{
+            get{
+                return new Point(BoardDimensions.Width / 2 , BoardDimensions.Height / 2 );
+            }
+        }
+
+        public PuzzleBobble(Point pos, Size siz, float rad = 20f) {
+            
+            //set up board
+            boardOffset = new Point(pos.X, pos.Y);
+            BoardDimensions = new Size(siz.Width, siz.Height);
+            Board = new Hexagon[BoardDimensions.Width][];
+            for (int x = 0; x < Board.Length; x++) {
+                Board[x] = new Hexagon[BoardDimensions.Height];
+                for (int y = 0; y < Board.Length; y++) {
+                    Board[x][y] = new Hexagon(rad, pos.X, pos.Y);
+                    Board[x][y].Radius = rad;
+                    Board[x][y].xIndexer = x;
+                    Board[x][y].yIndexer = y;
+                }
+            }
+
+        }
+        public void Initialize() {
+
+        }
+        public void Update(float deltaTime) {
+
+        }
+
+        public void HighlightNeighbors(Graphics g,float xOffset, float yOffset, Hexagon[][] board,Point mouse) {
+            Point MousePosition = new Point(mouse.X, mouse.Y);
+            // Get the X, Y (board indices) of the mouse position
+            Point mouseIndex = Hexagon.TileAt(new Point(MousePosition.X,MousePosition.Y), BallRadius,xOffset,yOffset);
+             // Make sure mouseIndex is in bounds
+            if (mouseIndex.X >= 0 && mouseIndex.Y >= 0 && mouseIndex.X < BoardDimensions.Width && mouseIndex.Y < BoardDimensions.Height) {
+                // Get all neighbors
+                List<Point> neighbors = board[mouseIndex.X][mouseIndex.Y].GetNeighborIndixes();
+                // Loop trough all neighbors
+                foreach (Point p in neighbors) {
+                    // If neightbor is valid
+                    if (p.X >= 0 && p.Y >= 0 && p.X < BoardDimensions.Width && p.Y < BoardDimensions.Height) {
+                        // Draw neighbor
+                        board[p.X][p.Y].Draw(g, Pens.Green);
+
+                        Point nw = board[mouseIndex.X][mouseIndex.Y].GetNeighborIndex(Hexagon.Directions.NorthWest);
+                        if (p == nw) {
+                            board[nw.X][nw.Y].Draw(g, Pens.Yellow);
+                        }
+                    }
+                }
+                board[mouseIndex.X][mouseIndex.Y].Draw(g, Pens.Red);
+            }
+        }
+
+        public void Render(Graphics g){
+            //Draw Board
+            for (int x = 0; x < Board.Length; x++) {
+                for (int y = 0; y < Board[x].Length; y++) {
+                    Board[x][y].Draw(g, Pens.Blue);
+                }
+            }
+            g.DrawRectangle(Pens.Green,BoardArea.Rectangle);
+            int _x = (int)(Board[0][0].W*0.5f);
+            int _y = (int)(Board[0][0].H*0.5f);
+            RectangleF debug = new RectangleF(_x - BallRadius, _y - BallRadius, BallDiameter, BallDiameter);
+            g.DrawEllipse(Pens.Yellow, debug);
+        }
+    }
+}
